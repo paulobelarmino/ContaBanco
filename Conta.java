b70cecbbc9dacdb809ce38e513641e28c4feb5fe
paulobelarmino <pabelarmino@gmail.com>
2024-05-22 19:03:23 -0300

    Finalizado Projeto

--- Conta.java
+++ Conta.java
@@ -1,2 +1,36 @@
 public class Conta {
+
+   int numeroConta;
+   String agencia;
+   String nomeCliente;
+   Double saldo = 12025.25;
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
 }
--- ContaBanco.java
+++ ContaBanco.java
@@ -1,5 +1,30 @@
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.util.Scanner;
+
 public class ContaBanco {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("BEM-VIND AO TERMINAL TAC");
+        System.out.println("-------------------------");
+        System.out.print("Digite o seu nome: ");
+        String nomeCliente = scanner.next();
+        System.out.print("Digite o número de sua agência: ");
+        String agencia = scanner.next();
+        System.out.print("Digite o número da sua conta: ");
+        int  numeroConta = scanner.nextInt();
+
+        Conta conta = new Conta();
+        conta.setNomeCliente(nomeCliente);
+        conta.setAgencia(agencia);
+        conta.setNumeroConta(numeroConta);
+
+        System.out.println("Olá ".concat(conta.getNomeCliente()) + ", estes são os dados de sua conta:");
+        System.out.println("Agência: ".concat(conta.getAgencia()));
+        System.out.println("Nº da Conta: ".concat(Integer.toString(conta.getNumeroConta())));
+        System.out.println("Seu saldo atual é R$".concat(Double.toString(conta.getSaldo())));
+
+
 
     }
 }
